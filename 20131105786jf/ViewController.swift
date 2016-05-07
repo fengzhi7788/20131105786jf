a6d5f93624b07ed7557e7fb41923d28ad642ba57
fengzhi7788 <fzfz@admindeiMac-62.local>
2016-05-07 12:49:48 +0800

    加了些按钮
    修改了一下数据库中的表，添加了个队名表，添加开始比赛按钮，意外退出按钮，两个队名文本框，两个viewcontrol界面用做跳转

--- 20131105786jf/ViewController.swift
+++ 20131105786jf/ViewController.swift
@@ -11,11 +11,13 @@ import UIKit
 class ViewController: UIViewController {
     var db:SQLiteDB!
 
+    @IBOutlet weak var name1: UITextField!
     @IBOutlet weak var text2: UITextField!
 
+    @IBOutlet weak var name2: UITextField!
     @IBOutlet weak var text1: UITextField!
-    var a=0
-    var b=0
+    var a:Int32=0
+    var b:Int32=0
     @IBAction func one(sender: UIButton) {
         a=a+1
         text1.text=("\(a)")
@@ -40,15 +42,26 @@ class ViewController: UIViewController {
         b=b+3
         text2.text=("\(b)")
     }
+    @IBAction func exitt(sender: UIButton) {
+        var x:Int32=0
+        var y:Int32=0
+        x=(text1.text! as NSString).intValue
+        y=(text2.text! as NSString).intValue
+        a=x
+        b=y
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //获取数据库实例
         db = SQLiteDB.sharedInstance()
         //如果表还不存在则创建表（其中uid为自增主键）
-        db.execute("create table if not exists t_user(uid integer primary key,uname varchar(20),mobile varchar(20))")
+        db.execute("create table if not exists score(uid integer primary key,Ascore varchar(20),Bscore varchar(20))")
+        db.execute("create table if not exists name(uid integer primary key,Aname varchar(20),Bname varchar(20))")
         //如果有数据则加载
         initUser()
+        
     }
     
     //点击保存
@@ -58,21 +71,21 @@ class ViewController: UIViewController {
     
     //从SQLite加载数据
     func initUser() {
-        let data = db.query("select * from t_user")
+        let data = db.query("select * from score")
         if data.count > 0 {
             //获取最后一行数据显示
             let user = data[data.count - 1]
-            text1.text = user["uname"] as? String
-            text1.text = user["mobile"] as? String
+            text1.text = user["Ascore"] as? String
+            text2.text = user["Bscore"] as? String
         }
     }
     
     //保存数据到SQLite
     func saveUser() {
-        let uname = self.text1.text!
-        let mobile = self.text1.text!
+        let Ascore = self.text1.text!
+        let Bscore = self.text2.text!
         //插入数据库，这里用到了esc字符编码函数，其实是调用bridge.m实现的
-        let sql = "insert into t_user(uname,mobile) values('\(uname)','\(mobile)')"
+        let sql = "insert into score(Ascore,Bscore) values('\(Ascore)','\(Bscore)')"
         print("sql: \(sql)")
         //通过封装的方法执行sql
         let result = db.execute(sql)
---
+++ 20131105786jf/jishi.swift
@@ -0,0 +1,9 @@
+//
+//  jishi.swift
+//  20131105786jf
+//
+//  Created by fz on 16/5/7.
+//  Copyright © 2016年 fz. All rights reserved.
+//
+
+import Foundation
